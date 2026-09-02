08d2ff6ac9c72aff926de53ff635ccb5599d1617
daffaaljibran28 <muchammadarief327@gmail.com>
2026-09-02 09:49:23 +0700

    Boolean Daffa

---
+++ lib/Muhammad Daffa Aljibran/boolean.dart
@@ -0,0 +1,10 @@
+void main() {
+  bool isRaining = true;
+  bool isSunny = false;
+
+  if (isRaining) {
+    print('Bawa payung, hari ini hujan!');
+  } else {
+    print('Tidak perlu bawa payung');
+  }
+}
