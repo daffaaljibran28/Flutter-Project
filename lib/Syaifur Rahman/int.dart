3a5d4d980c34b4ad1484d8d162ca02d46f958969
syaifur rahman <rahmansaifur1002@gmail.com>
2026-09-02 11:09:15 +0700

    menambahkan folder Syaifur Rahman dan percobaan int.dart

---
+++ lib/Syaifur Rahman/int.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  int jumlahApel = 5;
+  int jumlahJeruk = 3;
+
+  int totalBuah = jumlahApel + jumlahJeruk;
+
+  print('Jumlah apel: $jumlahApel');
+  print('Jumlah jeruk: $jumlahJeruk');
+  print('Total buah: $totalBuah');
+
+  totalBuah = totalBuah + 2; 
+  print('Total buah setelah ditambah: $totalBuah');
+}
