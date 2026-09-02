ad92f363e45f70e755626ab6c63f4f36cfa05bbc
NAUFAL ANWAR <mnaufalanwar06@gmail.com>
2026-09-02 11:02:59 +0700

    membahkan var

---
+++ lib/Muhammad Naufal Anwar/var.dart
@@ -0,0 +1,13 @@
+void main() {
+
+  var nama = "Naufal"; 
+  var umur = 20; 
+  var tinggi = 175.5; 
+  var isStudent = true; 
+
+  // Menampilkan nilai ke layar
+  print("Nama: $nama");
+  print("Umur: $umur tahun");
+  print("Tinggi: $tinggi cm");
+  print("Status Pelajar: $isStudent");
+}
