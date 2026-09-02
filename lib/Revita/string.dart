4d42b0a1ccfcb849e54cf7ce3b1213c879a4b578
Revita <revitadwi0906@gmail.com>
2026-09-02 09:33:25 +0700

    baru

---
+++ lib/Revita/string.dart
@@ -0,0 +1,11 @@
+void main() {
+  
+  String nama = "Revita";
+  String prodi = "Teknik Informatika";
+  String jurusan = "Teknologi Informasi";
+
+  print("Nama: $nama");
+  print("Prodi: $prodi");
+  print("Jurusan: $jurusan");
+
+}
